dd36a5333a993e72292dff45f946ef4198643ac7
bilaz666 <victor.me.marc@gmail.com>
2021-04-01 20:36:24 +0200

    bug

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const { show, isEqual, distance } = require('./point')
 const p1 = new Point(1, 10)
 const p2 = new Point(10, 11)
@@ -6,6 +5,4 @@ const p2 = new Point(10, 11)
 p1.show()
 p2.show()
 console.log(p1.isEqual(p2))
-console.log(p1.distance(p2))
-//NaN
-
+console.log(p1.distance(p2))//console.log = NaN
--- point.js
+++ point.js
@@ -21,9 +21,14 @@ class Point {
 exports.show = this.show
 exports.isEqual = this.isEqual
 exports.distance = this.distance
+//l'export/import n'a pas l'air de marché
+
+/*
+const p1 = new Point(1, 10)
+const p2 = new Point(10, 11)
 
 p1.show()
 p2.show()
 console.log(p1.isEqual(p2))
 console.log(p1.distance(p2))
-//NaN
+*/
